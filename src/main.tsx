73f5546e8a39ef706e5dbe2a254d394820ba2657
Voldi Bill Paulo Ngangu <aagustpro@gmail.com>
2026-09-17 11:28:31 +0100

    Use rebuilt app entrypoint with visible onboarding flow

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app/App';
 import InstallPrompt from './components/InstallPrompt';
 import './index.css';
 
